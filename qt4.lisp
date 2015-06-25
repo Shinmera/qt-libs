67cd1b8beed8cd27c8584e6f15a59fb3499e6a58
Shinmera <shinmera@tymoon.eu>
2015-06-25 10:16:38 +0200

    Working to maybe make it possible to download the qt libraries themselves as well.

--- qt4.lisp
+++ qt4.lisp
@@ -8,10 +8,20 @@
 
 (asdf:defsystem :qt4
   :class build-system
-  :version "4.8.7")
+  :pversion "qt-libs1.1.0")
 
-(defmethod origin ((c (eql (asdf:find-system :qt4))) &key type)
-  NIL)
+(defmethod checksum ((system (eql (asdf:find-system :qt4))) &key type)
+  (when (equal (version system) "qt-libs1.1.0")
+    (case type
+      (:compiled
+       #+(and windows x86-64)
+       #(182 176 149 24 84 34 246 199 40 105 82 131 34 128 70 255 18 60 123 222 215 8
+         217 63 142 15 192 134 139 149 56 81 60 154 99 102 190 212 110 111 141 55 250
+         9 206 239 144 183 226 226 127 140 127 69 158 31 53 250 116 71 229 106 216 70)
+       #+(and windows x86)
+       #(215 80 58 140 199 236 63 55 20 214 169 72 179 211 216 138 39 238 140 49 22
+         170 14 56 62 74 146 150 225 173 68 115 55 70 227 72 119 123 3 16 44 225 146
+         156 221 113 36 170 13 240 159 72 108 90 122 152 197 192 243 45 5 103 210 136)))))
 
 (defmethod asdf:perform ((op generate-op) (c (eql (asdf:find-system :qt4))))
   NIL)
@@ -20,7 +30,5 @@
   (test-prerequisite "Qt4.8" "qmake-qt4" "qmake"))
 
 (defmethod asdf:output-files ((op install-op) (c (eql (asdf:find-system :qt4))))
-  #+unix (list #p"/usr/lib/"
-               #p"/usr/share/qt4/"
-               #p"/usr/include/qt4/"
-               #p"/usr/bin/"))
+  (append (call-next-method)
+          (list (shared-library-file :name "qtcore" :defaults (relative-dir "install" "lib")))))
