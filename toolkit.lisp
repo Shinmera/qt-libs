b39b910f278dea598a9d0915eeba2fb0182e0db6
Luís Borges de Oliveira <lbo@siscog.pt>
2019-11-15 23:56:24 +0000

    Replace QT-LIBS::SETENV with (SETF UIOP:GETENV) for portability
    In particular, this makes qt-libs work on Allegro CL.

--- toolkit.lisp
+++ toolkit.lisp
@@ -190,22 +190,14 @@
              ~&Got      ~a"
               file (checksum-string checksum) (checksum-string received)))))
 
-(defun setenv (envvar new-value)
-  #+sbcl (sb-posix:setenv envvar new-value 1)
-  #+ccl (ccl:setenv envvar new-value T)
-  #+ecl (ext:setenv envvar new-value)
-  #-(or sbcl ccl ecl) (warn "Don't know how to perform SETENV.~
-                           ~&Please set the environment variable ~s to ~s to ensure proper operation."
-                            envvar new-value)
-  new-value)
-
 (defun get-path (&optional (envvar "PATH"))
   (cl-ppcre:split #+windows ";+" #-windows ":+" (uiop:getenv envvar)))
 
 (defun set-path (paths &optional (envvar "PATH"))
-  (setenv envvar (etypecase paths
-                   (string paths)
-                   (list (format NIL (load-time-value (format NIL "~~{~~a~~^~a~~}" #+windows ";" #-windows ":")) paths)))))
+  (setf (uiop:getenv envvar)
+        (etypecase paths
+          (string paths)
+          (list (format NIL (load-time-value (format NIL "~~{~~a~~^~a~~}" #+windows ";" #-windows ":")) paths)))))
 
 (defun pushnew-path (path &optional (envvar "PATH"))
   (let ((path (etypecase path
